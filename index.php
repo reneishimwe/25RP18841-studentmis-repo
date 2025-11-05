ed07776c5d5fee8c98b1f9ad36d3e96ccb123534
reneishimwe <reneishimwe94@gmail.com>
2025-11-05 01:10:04 -0800

    index page is created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MIS</title>
+</head>
+<body>
+    <h1>Welcome to Student MIS</h1>
+    <div>
+        <a href="registration.php">Register</a> | 
+    </div>
+</body>
+</html>
