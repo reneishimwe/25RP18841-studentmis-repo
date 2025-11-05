0497adcbda7ff2433eaf2b03221841ee7ffd06c5
reneishimwe <reneishimwe94@gmail.com>
2025-11-05 01:19:00 -0800

    25rp18841,the form is added successfully

---
+++ registration.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>USER REGISTRATION</title>
+</head>
+<body>
+    <h2>USER REGISTRATION</h2>
+    <form method="POST" action="register_process.php">
+        <input type="text" name="name" placeholder="names" required><br>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
